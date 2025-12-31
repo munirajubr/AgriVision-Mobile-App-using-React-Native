dab8d5b6b1aa459630f3b89a0d101a42da46cf97
munirajgowda <munirajgowdaraj@gmail.com>
2025-12-31 12:29:59 +0530

    api update

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://agrivision-mobile-app-using-react-native.onrender.com";
-export const API_URL = "http://13.60.216.41";
+export const API_URL = "https://agrivision-mobile-app-using-react-native.onrender.com";
+// export const API_URL = "http://13.60.216.41/api";
